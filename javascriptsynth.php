092e82fc11a0eea1f7b6453c213af6309ed5ab97
Apalis <gcoulby@googlemail.com>
2015-08-13 16:57:05 +0400

    Reorganised Structure
    Reorganised Structure

--- javascriptsynth.php
+++ javascriptsynth.php
@@ -10,22 +10,22 @@
                     <h5>Wave Form</h5>
                     <div class="ledLabel">
                         <input type="radio" id="osc1Led0" data-position="0" name="osc1Led" data-waveform="sine" data-osc="1" onclick="setWaveForm(this)">
-                        <label for="osc1Led0"><span></span><img src="img/synth/sine.png" alt="sine wave"></label>
+                        <label for="osc1Led0"><span></span><img src="img/sine.png" alt="sine wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc1Led1" data-position="1" name="osc1Led" data-waveform="square" data-osc="1" onclick="setWaveForm(this)" checked>
-                        <label for="osc1Led1"><span></span><img src="img/synth/square.png" alt="square wave"></label>
+                        <label for="osc1Led1"><span></span><img src="img/square.png" alt="square wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc1Led2" data-position="2" name="osc1Led" data-waveform="sawtooth" data-osc="1" onclick="setWaveForm(this)">
-                        <label for="osc1Led2"><span></span><img src="img/synth/sawtooth.png" alt="sawtooth wave"></label>
+                        <label for="osc1Led2"><span></span><img src="img/sawtooth.png" alt="sawtooth wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc1Led3" data-position="3" name="osc1Led" data-waveform="triangle" data-osc="1" onclick="setWaveForm(this)">
-                        <label for="osc1Led3"><span></span><img src="img/synth/triangle.png" alt="triangle wave"></label>
+                        <label for="osc1Led3"><span></span><img src="img/triangle.png" alt="triangle wave"></label>
                     </div>
                     <div class="ledLabel">
                         <div class="buttonDrop">
@@ -74,22 +74,22 @@
                     <h5>Wave Form</h5>
                     <div class="ledLabel">
                         <input type="radio" id="osc2Led0" data-position="0" name="osc2Led" data-waveform="sine" data-osc="2" onclick="setWaveForm(this)">
-                        <label for="osc2Led0"><span></span><img src="img/synth/sine.png" alt="sine wave"></label>
+                        <label for="osc2Led0"><span></span><img src="img/sine.png" alt="sine wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc2Led1" data-position="1" name="osc2Led" data-waveform="square" data-osc="2" onclick="setWaveForm(this)">
-                        <label for="osc2Led1"><span></span><img src="img/synth/square.png" alt="square wave"></label>
+                        <label for="osc2Led1"><span></span><img src="img/square.png" alt="square wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc2Led2" data-position="2" name="osc2Led" data-waveform="sawtooth" data-osc="2" onclick="setWaveForm(this)" checked>
-                        <label for="osc2Led2"><span></span><img src="img/synth/sawtooth.png" alt="sawtooth wave"></label>
+                        <label for="osc2Led2"><span></span><img src="img/sawtooth.png" alt="sawtooth wave"></label>
                     </div>
 
                     <div class="ledLabel">
                         <input type="radio" id="osc2Led3" data-position="3" name="osc2Led" data-waveform="triangle" data-osc="2" onclick="setWaveForm(this)">
-                        <label for="osc2Led3"><span></span><img src="img/synth/triangle.png" alt="triangle wave"></label>
+                        <label for="osc2Led3"><span></span><img src="img/triangle.png" alt="triangle wave"></label>
                     </div>
                     <div class="ledLabel">
                         <div class="buttonDrop">
@@ -143,8 +143,8 @@
         <div id="pianoKeys"></div>
     </div>
 
-    <script src="AddKeys.js"></script>
-    <script src="Context.js"></script>
-    <script src="Oscillator.js"></script>
-    <script src="Tones.js"></script>
+    <script src="js/AddKeys.js"></script>
+    <script src="js/Context.js"></script>
+    <script src="js/Oscillator.js"></script>
+    <script src="js/Tones.js"></script>
 </div>
